e01ee740ca212a7b3ea47f1dcb99dc9fc06547c3
Youssef <yelabd@purdue.edu>
2017-03-14 22:35:51 -0400

    Small fix

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,4 +1,4 @@
-def BubbleSort1(list):
+def bubbleSort1(list):
 	for i in range(len(list)-1,0,-1): #Outer for loop that starts at the end of the list and decrements
 		for j in range(i): #Inner for loop goes from 0 to i
 			if list[j] > list[j+1]: #Checks if the jth element in the list is greater than the j+1 element
@@ -7,5 +7,5 @@ def BubbleSort1(list):
 				list[j+1] = temp #Sets the j+1 element to temp
 
 toSort = [10,3,7,4,91,71]
-BubbleSort1(toSort)
+bubbleSort1(toSort)
 print(toSort)
